c70a3329f17d1f29ac9c30dc53e70c2ee4aaab74
Samip Gajurel <samipgajurel09@gmail.com>
2026-02-02 08:58:36 +0545

    Deploy ready

--- CodavatarInternTracking/settings.py
+++ CodavatarInternTracking/settings.py
@@ -55,6 +55,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 ROOT_URLCONF = "CodavatarInternTracking.urls"
 
