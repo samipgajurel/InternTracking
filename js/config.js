72d422b7a9418d7f423d665b8daa8c76a6ffbd66
Samip Gajurel <samipgajurel09@gmail.com>
2026-02-02 14:01:57 +0545

    Deploy: Render + Railway + Netlify config

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// frontend/js/config.js
-const API_BASE = "https://codavatarinterntracking-api.onrender.com/api";
-
-const ACCOUNTS_API = `${API_BASE}/accounts`;
-const INTERNSHIPS_API = `${API_BASE}`;
-const ATTENDANCE_API = `${API_BASE}/attendance`;
